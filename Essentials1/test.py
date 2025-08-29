bb88c9381cbe50bb665574020cbae3f8c4965eb4
HACKMYNUTTS <jorgemoruagonzalez@gmail.com>
2025-08-28 23:55:36 -0600

    Lectura y escritura de archivos raw

---
+++ Essentials1/test.py
@@ -0,0 +1,18 @@
+# // es para redondear hacia abajo
+s=1//2
+print(s)
+
+# ^ es para XOR
+# 1 ^ 0 = 1
+# 0 ^ 1 = 1
+
+a=1
+b=0
+a = a^b
+print(a)
+
+print("a", "b", "c", sep="sep")
+
+x=1
+y=2
+print(y%x)
---
+++ Essentials2/filehandling.py
@@ -0,0 +1,51 @@
+#manejo de archivos raw 
+#funcion open
+#open(filename, mode)
+#modes
+#r -> read
+#r+ -> read and write
+#w -> write
+#w+ -> read and write
+#a -> append
+
+myOutput = open("hello.txt", "w")
+myOutput.write("DONE by Jorge!\n")
+myOutput.writelines("Hello, world!\nThis is my first file written with Python.")
+myOutput.close()
+
+
+#append
+myOutput = open("hello.txt", "a")
+myOutput.write("\nThis is a new line added by append.")
+myOutput.close()
+
+#leer
+myInput = open("hello.txt", "r")
+print(myInput.read()) #este devuelve una cadena de string completa
+print (myInput.readlines()) #este devuelve una lista de las cadenas
+myOutput.close()
+
+#imprimir asi genera que el ultimo readlines devuelva una lista vacia
+#esto es debido a que el cursor se encuentra al final del archivo
+#esto lo podemos solucionar con seek
+#seek(offset, origin)
+
+myInput = open("hello.txt", "r")
+print(myInput.read()) 
+myInput.seek(0)
+print (myInput.readlines()) 
+myOutput.close()
+
+
+#leer mediante un bucle for
+
+myInput = open("hello.txt", "r")
+for line in myInput:
+    print(line)
+myInput.close()
+
+
+myInput = open("hello.txt", "r")
+for line in myInput.read():#devolvera cada una de las letras
+    print(line)
+myInput.close()
