fd7ba71f92317d9e6c03adde338084cec4c6b6e9
HACKMYNUTTS <jorgemoruagonzalez@gmail.com>
2025-09-03 20:27:54 -0600

    playing arround with os module and file handling

--- REAL PYTHON BOOK/filehandling2.py
+++ REAL PYTHON BOOK/filehandling2.py
@@ -10,8 +10,20 @@ import os
 #perfecto para almacenar urls
 path = r"C:/Users/jorge/OneDrive/Escritorio/DEV/Path2PythonMaster/REAL PYTHON BOOK/"
 
+#eliminar un archivo
+#syntax os.remove(path)
 
+os.remove(path+"shutil_folder/hi3.txt")
                     # ../es para subir una carpeta                 
-shutil.copy((path+"../hello.txt"),(path+"/shutil_folder/hi3.txt")) #aca jugamos con la ubicacion 
+shutil.copy((path+"../hello.txt"),(path+"/shutil_folder/hi4.txt")) #aca jugamos con la ubicacion 
                                                                    #el folder con el salto '../' 
                                                                    
+
+#Utilizar os para crear carpetas
+#syntax os.mkdir(path)
+os.mkdir(path+"test_folder1")
+
+#Utilizar os para eliminar carpetas
+#syntax os.rmdir(path)
+#cabe recalcar que solo eliminara carpetas vacias!!! 
+os.rmdir(path+"test_folder")
